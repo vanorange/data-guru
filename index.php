f34e91141d8923340634a030cab65d3d8e290659
vanorange <ucihathian@gmail.com>
2022-05-20 11:31:39 +0700

    upload pertama

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Data Guru</title>
+</head>
+<body>
+
+<div class="container">
+    <h1 style="text-align:center;">Data Guru</h1>
+    <br>
+    <!-- Tambah Data  -->
+    <button type="button" class="btn btn-success">Tambah Data</button>
+
+    <!-- Cari Data -->
+    <input style="margin-left:15px; width:300px;" type="text" placeholder="Cari Data">
+    <input class="btn-secondary" type="submit" value="Cari">
+
+    <br> <br>
+    <!-- TABEL -->
+    <table class="table caption-top">
+    <thead>
+        <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Tempat Lahir</th>
+        <th scope="col">Alamat</th>
+        <th scope="col">Jabatan</th>
+        <th scope="col">Opsi</th>
+        </tr>
+    </thead>
+    <tbody>
+    
+    </tbody>
+    </table>
+</div>
+  
+</body>
+</html>
